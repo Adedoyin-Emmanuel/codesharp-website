d5b018bfc060645b384e63fa8c796b0e74811aa6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-14 15:19:54 +0100

    chore: added site config

---
+++ app/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  domain: "https://codesharp.adedoyin.dev",
+  name: "Codesharp - Vscode Extension",
+  headline: "Codesharp  | Vscode Extension",
+  description:
+    "Instantly create C# files—classes, interfaces, structs, and more with one click!",
+  keywords: "codesharp, CodeSharp, C#, Vscode extension",
+  image: "https://codesharp.adedoyin.dev/logo.png",
+};
